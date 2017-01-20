cde3ea09767115a6c46ac621cc2f05ce17416cce
Steel <ajinkya@riva.co>
2017-01-21 00:14:00 +0530

    some refactoring

--- js/service.js
+++ js/service.js
@@ -1,6 +1,6 @@
 angular.module("weatherApp")
     .factory('weatherService', ['$http', function ($http) {
-        var apiKey = '3d8b309701a13f65b660fa2c64cdc517';//TODO constant
+        var API_KEY = '3d8b309701a13f65b660fa2c64cdc517', root_url = 'http://api.openweathermap.org/data/2.5/';
         var weatherInfo = {
             getWeather: getWeather,
             getForecast: getForecast
@@ -8,11 +8,11 @@ angular.module("weatherApp")
         return weatherInfo;
 
         function getWeather(cityName) {
-            return $http.get('http://api.openweathermap.org/data/2.5/weather', { params: { q: cityName, APPID: apiKey} });
+            return $http.get(root_url + 'weather', { params: { q: cityName, APPID: API_KEY} });
         }
 
         function getForecast(cityName) {
-            return $http.get('http://api.openweathermap.org/data/2.5/forecast/daily', { params: { q: cityName, APPID: apiKey, cnt: 4, units: "metric"} });
+            return $http.get(root_url + 'forecast/daily', { params: { q: cityName, APPID: API_KEY, cnt: 4, units: "metric"} });
         }
     }
     ]);
