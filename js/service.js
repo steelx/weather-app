7de8f2849827f8eba8db2f28c9b253afb0315491
Steel <ajinkya@riva.co>
2017-01-21 11:16:21 +0530

    weather map ssl link

--- js/service.js
+++ js/service.js
@@ -1,6 +1,6 @@
 angular.module("weatherApp")
     .factory('weatherService', ['$http', function ($http) {
-        var API_KEY = '3d8b309701a13f65b660fa2c64cdc517', root_url = 'http://api.openweathermap.org/data/2.5/';
+        var API_KEY = '3d8b309701a13f65b660fa2c64cdc517', root_url = '//api.openweathermap.org/data/2.5/';
         var weatherInfo = {
             getWeather: getWeather,
             getForecast: getForecast
